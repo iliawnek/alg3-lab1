4a8f017a025a6eeb7b688f84d6ed643d0ab4e550
Ken Li <iliawnek@gmail.com>
2016-10-13 19:22:07 +0100

    Output post-bfs vertex predecessors into text file

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,30 @@ public class Main {
         int numVertices = lineScanner.nextInt();
 
         // insert code here to build the graph from the input file
-
+        Graph graph = new Graph(numVertices);
+        for (int i = 0; i < graph.size(); i++) {
+            Vertex v = graph.getVertex(i);
+            lineScanner = new Scanner(in.nextLine());
+            for (int j = 0; j < graph.size(); j++) {
+                if (lineScanner.nextInt() == 1) {
+                    v.addToAdjList(j);
+                }
+            }
+        }
         reader.close();
 
-
         // conduct the breadth-first search
+        graph.bfs();
 
         String outputFileName = args[1];
         FileWriter writer = new FileWriter(outputFileName);
 
         // insert code here to output the predecessor information
+        for (int i = 0; i < graph.size(); i++) {
+            Vertex v = graph.getVertex(i);
+            writer.write(String.format("%d ", v.getPredecessor()));
+        }
 
         writer.close();
-
     }
-
 }
